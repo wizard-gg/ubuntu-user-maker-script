1c887aada72cae659a0015c858fd0c0d6d8c65ed
WiZ.GG <39291381+wizard-gg@users.noreply.github.com>
2023-05-13 07:33:10 +0330

    Create create_usaer.sh

---
+++ create_usaer.sh
@@ -0,0 +1,11 @@
+read -p "Enter Username: " name
+read -p "Expire month: " exdate
+
+useradd -M $name
+sudo passwd $name
+chage -E `date -d $exdate" month" +"%Y-%m-%d"` $name
+echo "-----------------------"
+echo -e 'Username: '$name
+echo -e 'Validate: '`date -d $exdate" month" +"%Y-%m-%d"`
+echo -e "-----------------------\n"
+
