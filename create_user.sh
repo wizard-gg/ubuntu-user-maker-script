c360b66a7d2f0d49894403782a4e6091eae3e555
WiZ.GG <39291381+wizard-gg@users.noreply.github.com>
2023-07-23 05:59:18 +0330

    Update create_user.sh

--- create_user.sh
+++ create_user.sh
@@ -1,7 +1,7 @@
 read -p "Enter Username: " name
 read -p "Expire month: " exdate
 
-useradd -r -s /bin/false $name
+useradd $name -m -d /home/$name -s /bin/true
 sudo passwd $name
 chage -E `date -d $exdate" month" +"%Y-%m-%d"` $name
 echo "-----------------------"
